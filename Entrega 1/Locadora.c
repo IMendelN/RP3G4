6bf78cc9400b0fc7f8a84fd94904f5f823a69421
Iago Mendes Nogueira <iagonogueira.aluno@unipampa.edu.br>
2022-05-09 13:59:14 -0300

    Implementação do metodo de busca de carros, interaçoes no painel inicial, busca de clientes e inicio do metodo de entrega de veiculo.

--- Entrega 1/Locadora.c
+++ Entrega 1/Locadora.c
@@ -3,8 +3,6 @@
 #include <locale.h>
 #include <stdio.h>
 
-//Main-----------------------------------------------------------------------------------------------------------
-
 //DADOS LOCAÇÃO-------------------------------------------------------------------------------------------------------
     typedef struct {
     char nome[30];
@@ -59,9 +57,7 @@ fscanf(arq, "%d", &numerodeLinha);
 do{
  r= fscanf(arq, "%29[^,],%d,%29[^,],%d,%d,\n",frota[i].nome, &frota[i].cnh, frota[i].placa, &frota[i].kmCat, &frota[i].dias); 
 
-if (r==5){
-    i++;
-}
+
 if (r!=5 && !feof(arq)){
     printf("Formato invalido");
     return 1;
@@ -82,37 +78,71 @@ printf("%s,%d,%s,%d,%d,\n", frota[i].nome, frota[i].cnh, frota[i].placa, frota[i
 
     fclose(arq);
 }
+//Main-----------------------------------------------------------------------------------------------------------
 int main(){
     locacao*frota;
     frota=malloc(sizeof(locacao)*100);
 //PAINEL INICIAL------------------------------------------------------------------------------------------------
-    ArqLoc(frota);
+    
     printf("Bem vindo a locadora de carros da Unipampa.\n");
-    printf("Selecione uma das opções abaixo para prosseguir.\n");
-    printf("Opção 1: Realizar uma locação de veículo. \n");
-    printf("Opção 2: Finalizar uma locação de veículo. \n");
-    printf("Opção 3: Finalizar o programa.\n");
+    printf("Selecione uma das opcoes abaixo para prosseguir.\n");
+    printf("Opcao 1: Realizar uma locacao de veiculo. \n");
+    printf("Opcao 2: Finalizar uma locacao de veiculo. \n");
+    printf("Opcao 3: Finalizar o programa.\n");
     
     int op;
     scanf("%d", &op);
-    
+       
     switch (op){
     case 1 : 
-        printf("Locação selecionado.");
+        
+        printf("Locacao selecionado.\n");
+        printf("Qual acao deseja realizar?\n");
+        printf("Opcao 1: Consultar Locacoes.\n");
+        printf("Opcao 2: Realizar uma locacao.\n");
+        int op2;
+        scanf("%d", &op2);
+        switch (op2){
+        case 1:
+            printf("Locacoes em Andamento:\n");
+            ArqLoc(frota);
+            break;
+        case 2:
+            printf("Realizar uma Locacao:\n");
+            printf("Digite o codigo da sua CNH:\n");
+            int novaCnh;
+            scanf("%d", &novaCnh);
+            printf("Digite seu nome:\n");
+            char novoNome;
+            scanf("%s", novoNome);
+            printf("Cadastro Realizado com sucesso.");
+            break;
+        default:
+            printf("Digite um valor válido.");
+            break;
+        }
         break;
-    case 2 :
-        printf("Finalizar locação selecionado");
+    case 2 : 
+        printf("Finalizar locacao selecionado\n");
+        printf("Informe o nome do cliente: \n");
+        int codLoc;
+        scanf("%d", &codLoc);
         break;
-    case 3 :
+    
+    case 3 : 
         printf("Programa finalizado. ");
         break;
+    
     default:
-        printf("Digite um valor válido. ");
+        printf("Digite um valor valido. ");
         break;
-    }
+    
+    
     system("pause");
     return 0;
     
+    }
 }
+
     
 
