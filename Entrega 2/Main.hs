182c1f1bf7ffc5d2afffd0f99b8a1e4478059622
Matheus <matheusboeira.aluno@unipampa.edu.br>
2022-05-31 13:12:58 -0300

    leitura de arquivo (incompleto)

--- Entrega 2/Main.hs
+++ Entrega 2/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import Dependencies.Import
+import System.IO
+
 import Menu.Menu
+import Championship.ReadFile
 
 main :: IO ()
-main = do
-    menu
+main = do 
+    readOnly
+
+---
+--- Versão que apenas imprime o conteúdo do arquivo.
+--- OBS.: Apenas para testes. Quando estiver pronta,
+--- esta será redirecionada a um módulo.
+---
+readOnly :: IO ()
+readOnly = do
+    content <- System.IO.readFile "Championship\\database\\result.csv"
+    putStrLn content
