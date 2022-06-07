e968f71001608d6565e8cfe697db42b0bded9df0
Matheus <matheusboeira.aluno@unipampa.edu.br>
2022-06-07 04:01:28 -0300

    adicionado mais duas cores

--- Entrega 2/src/Utils/Utils.hs
+++ Entrega 2/src/Utils/Utils.hs
@@ -14,6 +14,8 @@ green :: String
 yellow :: String
 blue :: String
 purple :: String
+cyan :: String
+white :: String
 reset :: String
 
 ---
@@ -24,4 +26,6 @@ green = "\27[1;32m"
 yellow = "\27[1;33m"
 blue = "\27[1;34m"
 purple = "\27[1;35m"
+cyan = "\27[1;36m"
+white = "\27[1;37m"
 reset = "\27[1;0m"
