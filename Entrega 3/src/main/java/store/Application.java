2eac6fccf4b3c8772d58dbc748b25cbbb4a685df
Matheus <matheusboeira.aluno@unipampa.edu.br>
2022-06-28 04:18:43 -0300

    feat: connection mysql and mocked data test

--- Entrega 3/src/main/java/store/Application.java
+++ Entrega 3/src/main/java/store/Application.java
@@ -1,11 +1,24 @@
 package store;
 
+import java.sql.Connection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbc;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\n\n...\n\n");
 	}
 }
