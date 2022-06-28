6b7ddc9fa4313f1b6d09cd64acbdffbca460d1a2
Guilherme de Lima de Menezes <glmguilima2255@gmail.com>
2022-06-28 14:22:54 -0300

    Merge branch 'guilherme' of https://gitlab.unipampa.edu.br/ales/resolucao-de-problemas-iii-2022.1/grupo-04 into guilherme

--- Entrega 3/src/main/java/store/models/Game.java
+++ Entrega 3/src/main/java/store/models/Game.java
@@ -1,5 +1,87 @@
 package store.models;
 
+import java.util.Date;
+import javax.persistence.Entity;
+
+@Entity
 public class Game {
+    Integer id, user_rating, role;
+    String title, description, developer, publisher;
+    Double price;
+    Date release_date;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUser_rating() {
+        return this.user_rating;
+    }
+
+    public void setUser_rating(Integer user_rating) {
+        this.user_rating = user_rating;
+    }
+
+    public Integer getRole() {
+        return this.role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeveloper() {
+        return this.developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getRelease_date() {
+        return this.release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
     
 }
