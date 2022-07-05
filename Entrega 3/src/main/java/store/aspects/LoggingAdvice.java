3cada12cade359efae14f0125448a494f1293e0e
Matheus <matheusboeira.aluno@unipampa.edu.br>
2022-07-05 17:15:57 -0300

    aspect j...

--- Entrega 3/src/main/java/store/aspects/LoggingAdvice.java
+++ Entrega 3/src/main/java/store/aspects/LoggingAdvice.java
@@ -2,8 +2,12 @@ package store.aspects;
 
 import java.time.LocalDateTime;
 
+import javax.servlet.http.HttpSession;
+
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import store.utils.App;
@@ -13,12 +17,26 @@ import store.utils.enums.Color;
 @Aspect
 @Component
 public class LoggingAdvice {
-    
-    @After("execution(* store.controllers.*.*(..))")
-    public void logging() {
+    @Autowired
+    private HttpSession session;
+
+    @Pointcut("execution(store.controllers.*)")
+    public void controllers() {
+    }
+
+    public static void logger(String message) {
         App.printf(Color.YELLOW_BRIGHT, DateUtil.formatDateTime(LocalDateTime.now()) + "\t");
         App.printf(Color.GREEN, " INFO");
         App.printf(Color.PURPLE, " 8080\t");
-        App.println(Color.CYAN, "Alguma coisa aconteceu!");
+        App.printf(Color.RED, message);
+    }
+    
+    @After("execution(* store.controllers.*.*(..)) && !execution(* store.controllers.LoginController.*(..)))")
+    public String isLogged() {
+        if (session.getAttribute("logged") == null) {
+            logger("Permissão negada. Redirecionando para a tela inicial.\n");
+            return "redirect:/login";
+        }
+        return "";
     }
 }
