e194248ad1621c79f618aa059312228cfa8fb65f
Guilherme de Lima de Menezes <glmguilima2255@gmail.com>
2022-07-12 17:06:12 -0300

    Reorganização do codigo dentro das areas específicas

--- Entrega 3/src/main/java/store/aspects/LoggingAdvice.java
+++ Entrega 3/src/main/java/store/aspects/LoggingAdvice.java
@@ -1,25 +1,37 @@
 package store.aspects;
 
-import java.time.LocalDateTime;
-
-import javax.servlet.http.HttpSession;
-
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
+import store.models.enums.TypeLog;
+import store.services.UserService;
+
+
+
 import store.utils.App;
 import store.utils.DateUtil;
 import store.utils.enums.Color;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class LoggingAdvice {
     @Autowired
     private HttpSession session;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private Logger logger;
+
 
     public static void logger(String message) {
         App.printf(Color.YELLOW_BRIGHT, DateUtil.formatDateTime(LocalDateTime.now()) + "\t");
@@ -45,4 +57,29 @@ public class LoggingAdvice {
         }
         return new ModelAndView("/index");
     }
+    @Pointcut("execution(* store.controllers.manager.*.*(..))")                 void pointcutManager() {}
+    @Pointcut("execution(* store.controllers.seller.*.*(..))")                  void pointcutSeller() {}
+
+    @Around("pointcutManager()")
+    public String managerArea(ProceedingJoinPoint joinPoint) throws Throwable {
+        Integer role = (Integer) session.getAttribute("role");
+        Long userId = (Long) session.getAttribute("userId");
+
+        if (role != null && userId != null && role >= 2) {
+            return (String) joinPoint.proceed();
+        }
+        logger.logAndSave(userId, TypeLog.ERROR, logger.MESSAGE_NOT_ADMIN, joinPoint);
+        return "Área permitida para gerentes ou administradores.";
+    }
+
+    @Around("pointcutSeller()")
+    public String sellerArea(ProceedingJoinPoint joinPoint) throws Throwable {
+        Integer role = (Integer) session.getAttribute("role");
+        Long userId = (Long) session.getAttribute("userId");
+
+        if (role != null && userId != null && role == 1 || role == 3) {
+            return (String) joinPoint.proceed();
+        }
+        logger.logAndSave(userId, TypeLog.ERROR, logger.MESSAGE_NOT_ADMIN, joinPoint);
+        return "Área permitida para vendedores ou administradores.";
 }
--- Entrega 3/src/main/java/store/controllers/manager/ManagerController.java
+++ Entrega 3/src/main/java/store/controllers/manager/ManagerController.java
@@ -1,17 +1,10 @@
 package store.controllers.manager;
 
-import javax.servlet.http.HttpSession;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aopalliance.intercept.Joinpoint;
-import org.aspectj.lang.annotation.Around;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Pointcut("execution(* store.controllers.manager.*.*(..))")
-void pointcutManager(){}
+
 
 @RestController
 @RequestMapping("/manageger/games")
@@ -35,18 +28,6 @@ public class ManagerController {
     @GetMapping("/{id}/delete")
     public String delete() {
         return "manager/users/?/delete";
-    }
-
-    @Around("pointcutManager()")
-    public String managerArea(ProceedingJoinPoint joinPoint) throws Throwable {
-        Integer role = (Integer) session.getAttribute("role");
-        Long userId = (Long) session.getAttribute("userId");
-
-        if (role != null && userId != null && role >= 2) {
-            return (String) joinPoint.proceed();
-        }
-        logger.logAndSave(userId, TypeLog.ERROR, logger.MESSAGE_NOT_ADMIN, joinPoint);
-        return "Área permitida para gerentes ou administradores.";
     }
 
 }
