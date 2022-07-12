b1accb336776cb15b02e32c5f5c932e736020ea2
Guilherme de Lima de Menezes <glmguilima2255@gmail.com>
2022-07-12 17:08:09 -0300

    Reorganização do codigo dentro das areas específicas

--- Entrega 3/src/main/java/store/controllers/seller/SellerController.java
+++ Entrega 3/src/main/java/store/controllers/seller/SellerController.java
@@ -1,21 +1,14 @@
 package store.controllers.seller;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Pointcut("execution(* store.controllers.seller.*.*(..))")
-void pointcutSeller() {}
-
-public class SellerController {
 
     @RestController
     @RequestMapping("/seller/games")
 
-    public class ManagerController {
+    public class SellerController {
         @GetMapping
         public String list() {
             return "seller/games";
@@ -36,16 +29,4 @@ public class SellerController {
             return "seller/users/?/delete";
         }
 
-    @Around("pointcutSeller()")
-    public String sellerArea(ProceedingJoinPoint joinPoint) throws Throwable {
-        Integer role = (Integer) session.getAttribute("role");
-        Long userId = (Long) session.getAttribute("userId");
-
-        if (role != null && userId != null && role == 1 || role == 3) {
-            return (String) joinPoint.proceed();
-        }
-        logger.logAndSave(userId, TypeLog.ERROR, logger.MESSAGE_NOT_ADMIN, joinPoint);
-        return "Área permitida para vendedores ou administradores.";
     }
-
-}
