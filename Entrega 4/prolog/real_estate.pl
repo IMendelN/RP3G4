25b9d61cad45f41030d91b932a8a5dbf0461f6d9
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2022-07-31 21:44:22 -0300

    .

--- Entrega 4/prolog/real_estate.pl
+++ Entrega 4/prolog/real_estate.pl
@@ -1,4 +1,5 @@
 :- dynamic list_set/2.
+:- dynamic client/3.
 
 % ------------------------------------------------------------------------------------------------
 % Profissões -> (Profissão).
@@ -142,7 +143,7 @@ list_average_agencies :-
     nl, forall(agency_in(Agency), average_value_by_agency(Agency)).
 
 % ------------------------------------------------------------------------------------------------
-% 2.6: Alterar informação de um determinado cliente. [NÃO TÁ FUNCIONANDO AINDA].
+% 2.6: Alterar informação de um determinado cliente. [OK].
 % ------------------------------------------------------------------------------------------------
 change_client(Code, NewAge, NewCareer) :-
     client(Code, _, _),
